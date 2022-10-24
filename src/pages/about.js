316ed47478d9bdd46e2335dac929dffa937fb42f
Chris Bui <sibulabu@gmail.com>
2022-10-24 09:48:12 -0500

    Finished Gatsby tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import {Link} from 'gatsby'
+
+const AboutPage = ()  => {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <Link to="/">Back to Home</Link>
+            <p>Hi there! Just testing out Gatsby</p>
+        </main>
+    )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
